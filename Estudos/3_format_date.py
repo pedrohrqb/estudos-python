ef5138028f60aeef46811439bbf350904e40ea0c
Pedro Henrique <pedrobezerraout@gmail.com>
2025-02-10 13:38:00 -0300

    estudos

--- Estudos/3_format_date.py
+++ Estudos/3_format_date.py
@@ -5,12 +5,12 @@ from datetime import datetime
 
 dh_atual = datetime.now()
 dh_str = "2023-10-20 10:20"
-mascara_ptbr = "%d/%m/%Y %a"
+mascara_ptbr = "%d/%m/%Y "
 mascara_en = "%Y-%m-%d %H:%M"
 
 #A função strftime pode cortar os restantes do elemento de uma variavel (por exemplo as horas)
 print(dh_atual.strftime(mascara_ptbr))
 
-data_convertida = datetime.strptime(dh_str, mascara_en)
+data_convertida = datetime.strptime(dh_str, mascara_ptbr)
 print(data_convertida)
 print(type(data_convertida))
